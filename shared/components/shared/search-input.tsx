b43d152f2d2b4cd8a264eb6baebcf73143da6024
Sadykoff <31240637+nozloy@users.noreply.github.com>
2024-08-03 20:43:59 +0300

    Update search-input.tsx
    Добавлено - обработка нажатия ESC при вводе данных в поисковкую строку.
    Добавлено - возобновление фокуса на поисковой строке после нажатия ESC и продолжении ввода
    Исправлено - появление полос иного цвета при наведении на первый и последний элемент списка, вызванных rounded стилем

--- shared/components/shared/search-input.tsx
+++ shared/components/shared/search-input.tsx
@@ -22,6 +22,29 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
     setFocused(false);
   });
 
+  // Обработчик нажатия ESC
+	useEffect(() => {
+		const handleEsc = (event: KeyboardEvent) => {
+			if (event.key === 'Escape') {
+				setFocused(false);
+				setSearchQuery('');
+			}
+		}
+
+		document.addEventListener('keydown', handleEsc)
+
+		// Удаляем обработчик при размонтировании компонента
+		return () => {
+			document.removeEventListener('keydown', handleEsc);
+		}
+	}, [])
+
+  // Возобновлять фокус при печати в строке поиска
+	const makeFocused = (value: string) => {
+		setSearchQuery(value);
+		setFocused(true);
+	}
+
   useDebounce(
     async () => {
       try {
@@ -55,20 +78,24 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
           placeholder="Найти пиццу..."
           onFocus={() => setFocused(true)}
           value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
+          onChange={(e) => makeFocused(e.target.value)}
         />
 
         {products.length > 0 && (
           <div
             className={cn(
-              'absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
+              'absolute w-full bg-white rounded-xl top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
               focused && 'visible opacity-100 top-12',
             )}>
             {products.map((product) => (
               <Link
                 onClick={onClickItem}
                 key={product.id}
-                className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
+                className={cn(
+									'flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10',
+									index === 0 && 'hover:rounded-t-xl', // Закругление верхнего края при наведении на первый элемент
+									index === products.length - 1 && 'hover:rounded-b-xl', // Закругление нижнего края при наведении на последний элемент
+								)}
                 href={`/product/${product.id}`}>
                 <img className="rounded-sm h-8 w-8" src={product.imageUrl} alt={product.name} />
                 <span>{product.name}</span>
